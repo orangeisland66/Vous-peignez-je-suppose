e82ce284bd7d0d779885c8fcb29b0825a471b690
orangeisland66 <orangeisland66@gmail.com>
2025-05-08 10:06:01 +0800

    修改一点语法错误

--- backend/Repositories/GameRepository.cs
+++ backend/Repositories/GameRepository.cs
@@ -1,10 +1,12 @@
+using backend.Data;
 using Microsoft.AspNetCore.Components.Endpoints;
+using backend.Models;
 
 namespace Repositories.GameRepository
 {
     public class GameRepository
 {
-    private DbContext _context;
+    private OurDbContext _context;
 
     public void GetRoomByIdAsync(int id)
     {
